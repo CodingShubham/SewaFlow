8b4a354c98db6988ea45715c7757045840d45e87
Shubham <ss9824473@gmail.com>
2026-06-19 16:31:21 +0530

    Added authentication system with signup, login and auth middleware

---
+++ server/Middlewares/authMiddleware.js
@@ -0,0 +1,42 @@
+
+const JWT=require("jsonwebtoken")
+const User=require("../Model/User")
+
+const authUser= async(req,res,next)=>{
+
+    //authUser Middleware for checking the correct User for login and other Routes
+try{
+
+    const{token}=req.cookies;
+
+    if(!token){
+        return res.status(401).send("Please login again")
+    }
+
+    const decodeObj=await JWT.verify(token,process.env.JWT_SECRET);
+    const{id}=decodeObj;
+
+    const user=await User.findById(id);
+    
+    if(!user){
+        throw new Error("User Not Found ")
+    }
+
+    req.user=user;
+    next();
+
+
+}
+
+
+catch(err){
+
+    res.status(500).json({error:err.message});
+
+}
+
+
+
+}
+
+module.exports=authUser;
--- server/app.js
+++ server/app.js
@@ -1,14 +1,29 @@
 const express=require("express");
 const dotenv=require("dotenv")
+const cors = require("cors");
 dotenv.config();
 const app=express();
+const cookieParser = require('cookie-parser');
+const authRoutes = require('./routes/authRoutes');
+const connectDB=require("./config/db")
+const auth=require("./Middlewares/authMiddleware")
+
+app.use(cors());
+app.use(express.json());
+
+app.use(cookieParser());
 
 const PORT=process.env.PORT
 
+connectDB();
 
 app.get("/",(req,res)=>{
     res.send("working")
 })
+
+
+app.use('/api/auth', authRoutes);
+
 
 app.listen(PORT,()=>{
     console.log(`server listening on ${PORT}`);
